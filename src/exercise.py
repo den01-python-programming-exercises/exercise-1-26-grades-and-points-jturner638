edfa40f2339405b81f5f9bb965e11a04f1d65b9f
Justin Turner <mrfathed@protonmail.com>
2021-12-04 11:38:46 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,25 @@
 def main():
     #write your code below this line
+    score = int(input("Give points [0-100]:"))
+
+    if score > 100:
+        grade = "incredible!"
+    elif score >= 90:
+        grade = 5
+    elif score >= 80:
+        grade = 4
+    elif score >= 70:
+        grade = 3
+    elif score >= 60:
+        grade = 2
+    elif score >= 50:
+        grade = 1
+    elif score >= 0:
+        grade = "failed"
+    else:
+        grade = "impossible!"
+
+    print("Grade: {}".format(grade))
 
 if __name__ == '__main__':
     main()
